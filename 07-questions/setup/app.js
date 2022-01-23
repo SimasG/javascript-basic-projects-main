999ed4e462cebe2cfd9a048db89252b35ed1089a
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-23 23:24:49 +0200

    finished 07-questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,15 +1,8 @@
-//using selectors inside the element
-// traversing the dom
-
 const faqs = document.querySelectorAll('[data-faq-target], [data-faq-icon-plus], [data-faq-icon-minus]');
 
 const faqContents = document.querySelectorAll("[data-faq-content]");
 const plusIcons = document.querySelectorAll("[data-faq-icon-plus-child]");
 const minusIcons = document.querySelectorAll("[data-faq-icon-minus-child]");
-
-
-
-
 
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
@@ -20,18 +13,8 @@ faqs.forEach(faq => {
         const plusIcon = document.querySelector(faq.dataset.faqIconPlus);
         const minusIcon = document.querySelector(faq.dataset.faqIconMinus);
 
-        // Making sure that all other icons & paragraphs are "closed" once we click on a new faq
-        faqContents.forEach(faqContent => {
-            faqContent.classList.remove("active");
-        })
-
-        plusIcons.forEach(iconPlus => {
-            iconPlus.classList.remove("active");
-        });
-
-        minusIcons.forEach(iconMinus => {
-            iconMinus.classList.remove("active");
-        });
+        // Making sure that all other icons & paragraphs are "closed" once we click on a new faq 
+        // & activating the clicked faq elements
         
         // Adding the paragraph
         if (faqTarget.classList.contains("active")) {
@@ -53,20 +36,17 @@ faqs.forEach(faq => {
         } else {
             minusIcon.classList.add("active");
         };
-
-
-
-
-        // Adding the paragraph
-        // faqTarget.classList.toggle("active")
-
-        // Addind the plus icon
-        // plusIcon.classList.toggle("active");
-
-        // Adding the minus icon
-        // minusIcon.classList.toggle("active");
     })
 })
+
+// Find out how to abstract the 3 if else statements into a function
+function currentFaq(e) {
+    if (e.classList.contains("active")) {
+        e.classList.remove("active");
+    } else {
+        e.classList.add("active");
+    };    
+}
 
 // Issue: once toggled, I cannot untoggle the icon by simply clicking on it.
 // Reason: When I click on the icon for the second time, I should remove the "active" class.
@@ -75,3 +55,17 @@ faqs.forEach(faq => {
 // That's why I can never remove it just by clicking on it.
 
 // I probably need if statements
+
+
+
+        // faqContents.forEach(faqContent => {
+        //     faqContent.classList.remove("active");
+        // })
+
+        // plusIcons.forEach(iconPlus => {
+        //     iconPlus.classList.remove("active");
+        // });
+
+        // minusIcons.forEach(iconMinus => {
+        //     iconMinus.classList.remove("active");
+        // });
