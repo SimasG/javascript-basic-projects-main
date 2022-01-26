a69da70e686803bb9caaecb111e22d47ba87d95e
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-26 15:29:27 +0200

    made progress on 10-scroll

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -13,18 +13,42 @@ const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', () => {
-    linksContainer.classList.toggle('show-links');
-    containerHeight = linksContainer.getBoundingClientRect();
-    // console.log(containerHeight);
+    containerHeight = linksContainer.getBoundingClientRect().height;
+    // console.log(containerHeight)
 
-    linksHeight = links.getBoundingClientRect();
-    console.log(linksHeight);
+    linksHeight = links.getBoundingClientRect().height;
+    // console.log(linksHeight);
 
-    // once the navbar is open (.show-links class is on), 
-    // the container height has to be the same as linksHeight
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
 })
 
+// For example, I couldn't tell if declaring globally or inside windor.addEventListener is better
+const navbar = document.querySelector('nav');
+const topLink = document.querySelector('.top-link');
+
+
 // ********** fixed navbar ************
+window.addEventListener('scroll', () => {
+    let scrollHeight = window.pageYOffset;
+
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav')
+    } else {
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link')
+    } else {
+        topLink.classList.remove('show-link')
+    }
+})
 
 // ********** smooth scroll ************
 // select links
